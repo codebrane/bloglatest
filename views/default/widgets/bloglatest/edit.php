7713fc2825e82f197d470e25a2d9e2c5bad735e1
Alistair Young <alistair@codebrane.com>
2010-03-25 10:48:36 +0000

    Added Wire Posts support

--- views/default/widgets/bloglatest/edit.php
+++ views/default/widgets/bloglatest/edit.php
@@ -14,6 +14,23 @@ require_once($CONFIG->pluginspath . "bloglatest/util/utils.php");
 ?>
 <p>
 <?
+/*
+ * Post type. Blog, Wire, etc...
+ */
+echo elgg_echo('bloglatest:view:edit:posttype:text').":";
+?>
+<select name="params[posttype]">
+	<option value="<?php echo POST_TYPE_ALL; ?>" <?php if($vars['entity']->posttype == POST_TYPE_ALL) echo "SELECTED"; ?>><?php echo elgg_echo('bloglatest:view:edit:posttype:all:text') ?></option>
+	<option value="<?php echo POST_TYPE_BLOG; ?>" <?php if($vars['entity']->posttype == POST_TYPE_BLOG) echo "SELECTED"; ?>><?php echo elgg_echo('bloglatest:view:edit:posttype:blog:text') ?></option>
+	<option value="<?php echo POST_TYPE_WIRE; ?>" <?php if($vars['entity']->posttype == POST_TYPE_WIRE) echo "SELECTED"; ?>><?php echo elgg_echo('bloglatest:view:edit:posttype:wire:text') ?></option>
+</select>
+</p>
+
+<p>
+<?
+/*
+ * Filter on username
+ */
 echo elgg_echo('bloglatest:view:edit:username:text').":";
 echo elgg_view('input/text', array('internalname' => 'params[username]', 
                                    'value' => $vars['entity']->username));
@@ -22,6 +39,9 @@ echo elgg_view('input/text', array('internalname' => 'params[username]',
 
 <p>
 <?
+/*
+ * Filter on tags
+ */
 echo elgg_echo('bloglatest:view:edit:tags:text').":";
 echo elgg_view('input/text', array('internalname' => 'params[tags]', 
                                    'value' => $vars['entity']->tags));
@@ -30,6 +50,9 @@ echo elgg_view('input/text', array('internalname' => 'params[tags]',
 
 <p>
 <?
+/*
+ * Number of posts to show
+ */
 echo elgg_echo('bloglatest:view:edit:noofposts:text').":";
 if (($vars['entity']->no_of_posts == nil) || ($vars['entity']->no_of_posts == "")) $vars['entity']->no_of_posts = MAX_LATEST_LIMIT;
 echo elgg_view('input/text', array('internalname' => 'params[no_of_posts]', 
@@ -40,6 +63,9 @@ echo elgg_view('input/text', array('internalname' => 'params[no_of_posts]',
 
 <p>
 <?
+/*
+ * Length of the excerpt of each post to display
+ */
 echo elgg_echo('bloglatest:view:edit:excerptlength:text').":";
 if (($vars['entity']->excerpt_length == nil) || ($vars['entity']->excerpt_length == "")) $vars['entity']->excerpt_length = MAX_EXCERPT_LENGTH;
 echo elgg_view('input/text', array('internalname' => 'params[excerpt_length]', 
